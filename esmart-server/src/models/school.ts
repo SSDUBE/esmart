2b918f1a02d9d32d4b5b79c991393c1beb1d7bb3
Sindiso <sdubesindiso@gmail.com>
2022-04-01 15:48:58 +0200

    fix: add if school is active

--- esmart-server/src/models/school.ts
+++ esmart-server/src/models/school.ts
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 export interface ISchool {
   _id: mongoose.Types.ObjectId;
   name: string;
+  active: boolean;
 }
 
 const SchoolSchema = new mongoose.Schema<ISchool>(
   {
     name: { type: String, required: true },
+    active: { type: Boolean, required: true, default: true },
   },
   {
     timestamps: true,
