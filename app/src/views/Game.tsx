194405383be3502e92bf9a6a215b1f65eff09585
Sindiso <sdubesindiso@gmail.com>
2022-05-23 14:27:42 +0200

    gifted chat

--- app/src/views/Game.tsx
+++ app/src/views/Game.tsx
@@ -2,38 +2,56 @@ import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet, TextInput } from 'react-native';
 import { io, Socket } from 'socket.io-client';
 import { AppContext } from '../context/context';
+import { GiftedChat } from 'react-native-gifted-chat';
 
-const socket = io('http://192.168.8.127:4000');
+const socket = io('http://192.168.101.201:4000');
+
+interface IMessage {
+  _id: number;
+  text: string;
+  createdAt: Date;
+  user: {
+    _id: number;
+    name: string;
+    avatar: string;
+  };
+}
 
 export const Game = () => {
-  const context = React.useContext(AppContext)
-  const [chatMsg, setChatMsg] = React.useState('');
-  const [chatMsgs, setChatMsgs] = React.useState<any>([]);
+  const context = React.useContext(AppContext);
+  // const [chatMsg, setChatMsg] = React.useState('');
+  // const [chatMsgs, setChatMsgs] = React.useState<any>([]);
+
+  const [messages, setMessages] = React.useState<IMessage[]>([]);
 
   React.useEffect(() => {
-    (function () {
-      // const is = socket.connect()
-      // console.log('issss ', is)
-      
-    })();
-
     socket.on('chat message', (msg) => {
-      setChatMsgs([...chatMsgs, msg]);
+      console.log('msg ', msg)
+      setMessages([...msg]);
+      // setChatMsgs([...chatMsgs, msg]);
     });
   }, []);
 
   function submitChagMsg() {
-    socket.emit('chat message', chatMsg);
-    setChatMsg('');
+    // socket.emit('chat message', chatMsg);
+    // setChatMsg('');
   }
 
-  const chatMessages = chatMsgs.map((msg: string, i: number) => (
-    <Text key={i}>{msg}</Text>
-  ));
+  // const chatMessages = chatMsgs.map((msg: string, i: number) => (
+  //   <Text key={i}>{msg}</Text>
+  // ));
+
+  const onSend = React.useCallback((messages = []) => {
+    setMessages((previousMessages) => {
+      socket.emit('chat message', messages)
+      return GiftedChat.append(previousMessages, messages)
+    });
+    // socket.emit('chat message', messages)
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
+      {/* <View style={styles.container}>
         <TextInput
           style={{ height: 40, borderWidth: 2, marginTop: 50 }}
           autoCorrect={false}
@@ -42,7 +60,21 @@ export const Game = () => {
           onChangeText={(msg) => setChatMsg(msg)}
         />
         {chatMessages}
-      </View>
+      </View> */}
+      <GiftedChat
+        scrollToBottom
+        infiniteScroll
+        // loadEarlier
+        alwaysShowSend
+        // renderUsernameOnMessage
+        inverted={true}
+        // showUserAvatar
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     </SafeAreaView>
   );
 };
